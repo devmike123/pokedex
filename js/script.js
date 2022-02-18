1c4f3946f27a6bb398df2b13688eb0f83b54577f
Aniket Bhattacharjee <appsoftlimited@gmail.com>
2022-02-18 09:40:50 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -42,7 +42,6 @@ function createPokemonCard(pokemon) {
 	const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
 	const color = colors[type];
         const small = pokemon.name;
-	const big = types[0].type.name[0].toUpperCase() + types[0].type.name.slice(1);
 	
 	pokemonEl.style.backgroundColor = color;
 
@@ -57,7 +56,7 @@ function createPokemonCard(pokemon) {
 							.toString()
 							.padStart(3, '0')}</span>
             <h3 class="name">${name}</h3>
-            <small class="type">Type: <span>${big}</span></small>
+            <small class="type">Type: <span>${type}</span></small>
         </div>
     `;
 
